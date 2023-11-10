8a70cba41c33321a0b92652464a7a6f84ba282ae
Jethro Baring <80811809+JethroBaring@users.noreply.github.com>
2023-11-10 18:22:46 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "launchlab",
         "USER": "root",
-        "PASSWORD": "Jethro#123",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3306",
         "OPTIONS": {
