5dee5d3e7a45a642cde0e3414ea0a9b616f121ab
Jethro Baring <baring.jeth@gmail.com>
2023-11-10 18:15:05 +0800

    added landing page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,23 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'flutter-blue': '#1C83E5',
+        'flutter-dark': '#0B213E'
+      },
+      fontFamily: {
+        'Inter': ['Inter', 'sans-serif']
+      },
+      boxShadow: ['dark'],
+      flex: {
+        '2' : '0 0 auto'
+      }
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "winter"],
+  },
 }
 
