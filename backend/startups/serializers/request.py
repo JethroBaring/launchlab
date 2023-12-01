de200399ec0e56e24c4f1a56631d4a7429b80c20
Jhudiel Muñoz <67956800+jhudiel0101@users.noreply.github.com>
2023-12-01 17:59:38 +0800

    set is_qualified as read_only

--- backend/startups/serializers/request.py
+++ backend/startups/serializers/request.py
@@ -4,6 +4,7 @@ from startups.serializers import base as startups_base_serializers
 
 
 class StartupRequestSerializer(startups_base_serializers.StartupBaseSerializer):
+    is_qualified = serializers.BooleanField(read_only=True)
     set_members = serializers.ListField(child=serializers.CharField(), write_only=True)
 
     class Meta:
